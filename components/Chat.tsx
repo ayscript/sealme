22d8b8e8eef1640384f939e9015e3fa702ce9eef
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-03-13 19:59:35 +0100

    new commit

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -11,10 +11,17 @@ const Chat: React.FC<ChatProps> = ({ children, dateCreated }) => {
     <p
       className={`bg-foreground text-sm text-text p-3 rounded m-2 max-w-72 sm:max-w-[50%] pb-[0] flex flex-col gap-2`}
       onTouchMove={(e) => {
-        (e.target as HTMLElement).style.transform = `translateX(${e.touches[0].clientX}px)`;
+        if (e.target === e.currentTarget) {
+          (e.target as HTMLElement).style.transform = `translateX(${e.touches[0].clientX}px)`;
+        }
+      }}
+      onTouchEnd={(e) => {
+        if (e.target === e.currentTarget) {
+          (e.target as HTMLElement).style.transform = 'translateX(0)';
+        }
       }}
     >
-      <span className='w-full text-ellipsis overflow-hidden inline-block'>
+      <span className='text-ellipsis overflow-hidden inline-block'>
         {typeof children === 'object' ? JSON.stringify(children) : children}
       </span>
       <span className='text-[8px] self-end'>{dateCreated}</span>
